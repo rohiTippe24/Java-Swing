a44a265f3ee0b5b3e5540736c9ec56b3df308676
Rohit Tippe <rohitippe24a@gmail.com>
2022-09-22 16:45:38 +0530

    Update cardLayout.java

--- cardLayout.java
+++ cardLayout.java
@@ -1,44 +1,66 @@
-//  Box Layout in Java
+//  CardLayout  :- only one content display on the screen
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class boxLayout {
+class myFrame extends JFrame implements ActionListener {
+  Container c;
+  JButton b1 , b2 , b3 , b4 , b5 ;
+  CardLayout cl ;
+  myFrame () {
+    c = this.getContentPane();
+    
+    cl  = new CardLayout();
+    c.setLayout(cl);
+    
+    b1 = new JButton("Page 1");
+    b2 = new JButton("Page 2");
+    b3 = new JButton("Page 3");
+    b4 = new JButton("Page 4");
+    b5 = new JButton("Page 5");
+    
+    b1.addActionListener(this);
+    b2.addActionListener(this);
+    b3.addActionListener(this);
+    b4.addActionListener(this);
+    b5.addActionListener(this);
   
+    c.add(b1 , "1");
+    c.add(b2 , "2");
+    c.add(b3 , "3");
+    c.add(b4 , "4");
+    c.add(b5 , "5");
+    
+      }
+  public void actionPerformed (ActionEvent e) {
+    
+    //  Card Layout Methods 
+    
+      cl.next(c);
+      // cl.previous(c);
+      // cl.first(c);
+      // cl.last(c);
+      // cl.show(c , "1");
+      
+      
+    } 
+    
+
+  
+}
+
+
+public class cardLayout {
   
   public static void main (String[] args) {
     
+    myFrame frame = new myFrame();
+    frame.setBounds(400 , 150 , 800 , 500);
+    frame.setTitle("CardLayout");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
    
-JFrame frame = new JFrame("BoxLayout");
-frame.setBounds(400 , 150 , 800 , 500);
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setVisible(true);
- 
- Container c = frame.getContentPane();
-
-// BoxLayout bl = new BoxLayout(Container , alignment axis);
- 
- BoxLayout bl = new BoxLayout(c , BoxLayout.Y_AXIS);
- c.setLayout(bl);
- 
- JButton b1 = new JButton("Button 1");
- JButton b2 = new JButton("Button 2");
- JButton b3 = new JButton("Button 3");
- JButton b4 = new JButton("Button 4");
- JButton b5 = new JButton("Button 5");
- 
-//  Set component alignment
- 
- b2.setAlignmentX(Component.CENTER_ALIGNMENT);
- b3.setAlignmentX(Component.RIGHT_ALIGNMENT);
- b4.setAlignmentX(Component.CENTER_ALIGNMENT);
- 
- c.add(b1);
- c.add(b2);
- c.add(b3);
- c.add(b4);
- c.add(b5);
-    
   }
   
 }
